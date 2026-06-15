4c433a3d9e160fb394addb360aad311786cf8380
cxlvnn <behruzturabekov19@gmail.com>
2026-06-16 02:24:34 +0500

    baby steps

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chatea</title>
+    @vite(['resources/css/app.css'])
+</head>
+
+<body>
+    <h1 class="text-green-900">Hello from blade</h1>
+</body>
+
+</html>
