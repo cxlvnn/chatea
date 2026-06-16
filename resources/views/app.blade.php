f05e83691221fdfe756eca0e4b53f3e479a037c0
cxlvnn <behruzturabekov19@gmail.com>
2026-06-16 16:34:03 +0500

    vue and inertia setup with typescript

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,15 +1,16 @@
+{{-- resources/views/app.blade.php --}}
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chatea</title>
-    @vite(['resources/css/app.css'])
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite(['resources/css/app.css', 'resources/js/app.ts'])
+    <x-inertia::head />
 </head>
 
 <body>
-    <h1 class="text-green-900">Hello from blade</h1>
+    <x-inertia::app />
 </body>
 
 </html>
