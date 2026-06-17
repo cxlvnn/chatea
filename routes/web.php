fa2e07fd75e5cd06729ab3a85659b6161c7b6697
cxlvnn <behruzturabekov19@gmail.com>
2026-06-17 17:54:50 +0500

    root uri redirect to chats

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,19 @@ use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return to_route('chat.index');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/chats', [ChatController::class, 'index'])->name('chat.index');
     Route::post('/chats/create', [ChatController::class, 'create'])->name('chat.create');
 });
 
-Route::get('/login', [AuthController::class, 'show_login'])->name('login.show');
-Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::get('/register', [AuthController::class, 'show_register'])->name('register.show');
-Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'show_login'])->name('login.show');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::get('/register', [AuthController::class, 'show_register'])->name('register.show');
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+});
 Route::post('/logout', [AuthController::class, 'destroy'])->name('logout')->middleware('auth');
