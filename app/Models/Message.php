d080516974b8a3b90ef605fe10b4a617ab8b9e8d
cxlvnn <behruzturabekov19@gmail.com>
2026-08-19 19:00:00 +0500

    introduced touches attribute on the child message model in order to update the timestamps of chat

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use Database\Factories\MessageFactory;
+use Illuminate\Database\Eloquent\Attributes\Touches;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+#[Touches(['chat'])]
 class Message extends Model
 {
     /** @use HasFactory<MessageFactory> */
